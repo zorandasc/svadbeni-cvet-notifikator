b44afe64aee747fc340a6538384e42b944251576
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2023-03-08 11:50:28 +0100

    added tokens

--- routes/expoPushTokens.js
+++ routes/expoPushTokens.js
@@ -5,6 +5,11 @@ const Joi = require("joi");
 const tokenStore = require("../store/tokens");
 const validateWith = require("../middleware/validation");
 
+router.get("/", (req, res) => {
+  const tokens = tokenStore.getTokens();
+  res.send(tokens);
+});
+
 router.post(
   "/",
   [validateWith({ token: Joi.string().required() })],
