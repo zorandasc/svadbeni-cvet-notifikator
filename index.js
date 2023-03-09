c54563bfd80b55c80f1ff846a2f4f5d98479108e
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2023-03-09 11:39:34 +0100

    cors

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ var cors = require("cors");
 const messages = require("./routes/messages");
 const expoPushTokens = require("./routes/expoPushTokens");
 
-app.use(cors());
+app.use(
+  cors(
+    cors({
+      origin: [
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "https://deluxe-stroopwafel-bfc24b.netlify.app",
+      ],
+    })
+  )
+);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(compression());
+//app.use(express.urlencoded({ extended: true }));
+//app.use(helmet());
+//app.use(compression());
 
 app.use("/api/expoPushTokens", expoPushTokens);
 app.use("/api/messages", messages);
